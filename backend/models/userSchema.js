40f12f779346075ac7fe8c1a960f68088309ff15
Navneet Kumar <navneetsinghpanwar90@gmail.com>
2023-03-02 12:25:58 +0530

    initial commit

---
+++ backend/models/userSchema.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+import AuthRoles from '../utils/AuthRoles';
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+require('dotenv').config()
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        maxlength: [30, "Maximum length allowed is 30 characters"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Minimum length is 8 characters"],
+        select: false
+    },
+    role: {
+        type: String,
+        enum: Object.values(AuthRoles),
+        default: AuthRoles.USER
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date
+},
+    {
+        timestamps: true
+    }
+);
+
+//encrypting the password - mongoose hooks
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+
+    next();
+})
+
+//mongoose methods
+userSchema.methods = {
+    //compare password
+    comparePassword: async function (enteredPwd) {
+        return await bcrypt.compare(enteredPwd, this.password)
+    },
+
+    //Generate JWT Token
+    getJwtToken: function () {
+        return JWT.sign(
+            {
+                _id: this._id,
+                role: this.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_EXPIRY || "30d"
+            }
+        )
+    }
+
+}
