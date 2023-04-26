264735530388a7386bf5baef8af35d6f89b503bc
Navneet Kumar <navneetsinghpanwar90@gmail.com>
2023-04-26 23:22:26 +0530

    added collection routes

---
+++ backend/Controllers/collectionControllers.js
@@ -0,0 +1,68 @@
+const Collection = require("../models/collectionSchema");
+const asyncHandler = require("../services/asyncHandler");
+
+
+//only admin can create collection
+
+//create a collection
+exports.createCollection = asyncHandler(async (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        throw new Error("Collection name is required")
+    }
+
+    const collection = await Collection.create({ name });
+
+    res.status(200).json({
+        success: true,
+        collection
+    })
+})
+
+//get all collection
+exports.getAllCollection = asyncHandler(async (_req, res) => {
+    const collections = await Collection.find();
+
+    if (!collections) {
+        throw new Error("There is no collection")
+    }
+
+    res.status(200).json({
+        success: true,
+        collections
+    })
+})
+
+//update collection
+exports.updateCollection = asyncHandler(async (req, res) => {
+
+    const collection = await Collection.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Collection updated successfully",
+        collection
+    })
+})
+
+
+//delete collection
+exports.deleteCollection = asyncHandler(async (req, res) => {
+    const collection = await Collection.findById(req.params.id);
+
+    if (!collection) {
+        throw new Error("No such collection is found")
+    }
+
+    await collection.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "Collection deleted successfully"
+    })
+})
---
+++ backend/routes/collectionRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { isLoggedIn, customRole } = require("../middlewares/authMiddleware");
+const { createCollection, updateCollection, deleteCollection, getAllCollection } = require("../Controllers/collectionControllers");
+
+router.get("/collections", isLoggedIn, getAllCollection);
+router.post("/collection/create", isLoggedIn, customRole("admin"), createCollection);
+router.put("/collection/:id", isLoggedIn, customRole("admin"), updateCollection);
+router.delete("/collection/:id", isLoggedIn, customRole("admin"), deleteCollection);
+
+
+module.exports = router;
